74f4c04c2aa8d3e495485ceed13c65f7c2cff51f
Thomas Galliker <thomas@bluewin.ch>
2026-03-27 22:24:37 +0100

    Add logging helpers

---
+++ Tests/Superdev.AspNetCore.Tests/Logging/TestOutputHelperLoggerFactory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Superdev.AspNetCore.Tests.Logging
+{
+    public class TestOutputHelperLoggerFactory : ILoggerFactory
+    {
+        private readonly ITestOutputHelper testOutputHelper;
+
+        public TestOutputHelperLoggerFactory(ITestOutputHelper testOutputHelper)
+        {
+            this.testOutputHelper = testOutputHelper ?? throw new ArgumentNullException(nameof(testOutputHelper));
+        }
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new TestOutputHelperLogger(this.testOutputHelper, categoryName);
+        }
+
+        public void AddProvider(ILoggerProvider provider)
+        {
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
